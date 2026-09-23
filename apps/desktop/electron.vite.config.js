b7a58a420a27690a849f5ce2ebb058f51ce98845
neilscodes <soham3113567@gmail.com>
2026-09-24 02:57:56 +0530

    upgrade

--- apps/desktop/electron.vite.config.js
+++ apps/desktop/electron.vite.config.js
@@ -1,10 +1,11 @@
 import { resolve } from 'path';
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
+import commonjs from '@rollup/plugin-commonjs';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()],
+    plugins: [externalizeDepsPlugin(), commonjs()],
     build: {
       outDir: 'out/main',
       rollupOptions: {
@@ -24,7 +25,7 @@ export default defineConfig({
     },
   },
   preload: {
-    plugins: [externalizeDepsPlugin()],
+    plugins: [externalizeDepsPlugin(), commonjs()],
     build: {
       outDir: 'out/preload',
       rollupOptions: {
